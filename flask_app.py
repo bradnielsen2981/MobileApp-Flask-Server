f6f1cb30ea0fe1b570b222f6e2652f5eccf339bb
Brad Nielsen <38126504+bradnielsen2981@users.noreply.github.com>
2022-05-19 09:13:15 +1000

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -2,11 +2,10 @@
 import uuid, sys, logging, math, time, os, re
 from flask import Flask, render_template, session, request, redirect, url_for, flash, jsonify
 from flask_cors import CORS #Needs to be installed to allow cross origin
-#from databaseinterface import Database
+from databaseinterface import Database
 from datetime import datetime
 import requests #needed for an external API
-#import globalvars
-#import helpers
+import helpers
 
 #---CONFIGURE APP---------------------------------------------------#
 sys.tracebacklimit = 2 #Level of python traceback - useful for reducing error text
@@ -14,9 +13,24 @@ app = Flask(__name__) #Creates the Flask Server Object
 #DELETE config from object
 CORS(app) #enables cross domain scripting protection
 
-@app.route('/', methods=['GET','POST'])
+# LOGIN SCRIPT
+@app.route('/login', methods=['GET','POST'])
 def login():
-    return "Hello from Flask"
+    log("Login")
+    data = { "success":False, "message":"Login unsuccessful"}
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        userdetails = DATABASE.ViewQuery("SELECT * FROM users WHERE email = ?", (email,))
+        app.logger.info(userdetails)
+        if userdetails:
+            user = userdetails[0]
+            data = { "success":True, "message":"Login successful"}
+            data['userid'] = user['userid']
+            # session['userid'] = user['userid']
+            data['permission'] = user['permission']
+            data['name'] = user['name']
+    return jsonify(data) #returns the data to the app
 
 #main method called web server application
 if __name__ == '__main__':
